80f3925d10698c58b8989fd02fdd4cc4795b22b5
Sam Davis <sam@sjd.co>
2016-05-09 21:40:25 +1000

    add page.php support

--- page.php
+++ page.php
@@ -1,3 +1,19 @@
 <?php get_header(); ?>
 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<section class="content-area">
+    <div class="row">
+        <div class="medium-8 medium-offset-2 columns">
+
+            <h1><?php the_title(); ?></h1>
+
+            <?php the_content(); ?>
+
+        </div>
+    </div>
+</section>
+
+<?php endwhile; endif; ?>
+
 <?php get_footer(); ?>
