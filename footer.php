c42292aa1eaab638364a985a39ed5ccb1a53a507
Sam Davis <sam@sjd.co>
2016-05-10 12:49:56 +1000

    add range to copyright year

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="row">
         <div class="large-12 columns">
-            <p>&copy; <?php echo Date('Y'); ?> <a href="http://sjd.co/" target="_blank">Sam Davis</a>&nbsp;&nbsp;
+            <p>&copy; 2015-<?php echo Date('Y'); ?> <a href="http://sjd.co/" target="_blank">Sam Davis</a>&nbsp;&nbsp;
             <a target="_blank" href="http://github.com/samjdavis13"><i class="fa fa-github fa-lg"></a></i>&nbsp;&nbsp;
             <a target="_blank" href="http://twitter.com/samjdavis"><i class="fa fa-twitter fa-lg"></i></a>
         </p>
