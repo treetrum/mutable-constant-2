db2523b1b7f9ef8c7c62b95b94717cf4ae8f6abf
Sam Davis <sam@sjd.co>
2016-10-29 13:19:23 +1100

    Better auto height handling when pagination is present.

--- js/app.js
+++ js/app.js
@@ -16,9 +16,11 @@ function makePostsMinHeight() {
     var totalHeight = $(window).height();
     var headerHeight = $('.header').height();
     var topnavHeight = $('.topnav').height();
+    var paginationHeight = $('.pagination').height();
+    var paginationPaddingBottom = parseInt($('.pagination').css('padding-bottom').replace(/[^-\d\.]/g, ''));
     var footerHeight = 41;
 
-    var minHeight = totalHeight - (headerHeight + topnavHeight + footerHeight);
+    var minHeight = totalHeight - (headerHeight + topnavHeight + footerHeight + paginationHeight + paginationPaddingBottom);
 
     $posts.each(function(index) {
         $(this).css({
